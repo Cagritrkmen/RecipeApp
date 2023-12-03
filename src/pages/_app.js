c4861e5958ed39c97bf055c63199cf86570fec47
Cagritrkmen <cagritrkmn33@gmail.com>
2023-12-03 21:54:34 +0300

    Add Dashboard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
+import DashbordLayout from '@/components/DashboardLayout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+  <DashbordLayout >
+    <Component {...pageProps} />
+  </DashbordLayout>)
 }
